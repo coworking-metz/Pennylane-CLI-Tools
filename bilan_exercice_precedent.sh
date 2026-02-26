02db224d6729ab2793c66fea881b6eaa58294765
Gilles FRANCOIS <gilles@sopress.net>
2026-02-26 17:02:54 +0100

    script pour faire le bialn année -1

---
+++ bilan_exercice_precedent.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+php pennylane_exporter.php --categories="Famille.Charges"
+php pennylane_exporter.php --categories="Famille.Investissements"
+php pennylane_exporter.php --categories="Famille.Revenu exceptionnel"
+php pennylane_exporter.php --categories="Famille.Revenu opérationnel"
+php pennylane_exporter.php --categories="Famille.Subventions"
